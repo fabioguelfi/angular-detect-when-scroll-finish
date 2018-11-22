5d660c6ed2209afc63a72638db968f0753046bef
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-22 08:06:35 -0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scroll';
+export class AppComponent implements AfterViewInit {
+
+  public ngAfterViewInit(): void {
+    this.methodScrollListener();
+  }
+
+  private methodScrollListener(): void {
+    let visiblity = 'invisible';
+    
+    // Create new IntersectionObserver
+    const io = new IntersectionObserver(entries => {
+      // Available data when an intersection happens
+      console.log(entries);
+      // Element enters the viewport
+      if (entries[0].intersectionRatio !== 0) {
+        visiblity = 'visible';
+        // Element leaves the viewport 
+      } else {
+        visiblity = 'invisible';
+      }
+      updateStatus(visiblity);
+    });
+
+    // Elements to be observed
+    const box1 = document.querySelector('.box1');
+
+    // Start observing .box
+    io.observe(box1);
+
+    // Just necessary for displaying the current status
+    function updateStatus(visiblity) {
+      console.log(`Update var on class component`, visiblity);
+    }
+
+  }
 }
